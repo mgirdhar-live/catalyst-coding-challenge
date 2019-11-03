72e5bf29da64557774f49714d9024d546cb146a9
Manish <m.girdhar@live.com>
2019-11-03 20:29:53 +0530

    import users and create table logic added

--- backend/user_upload.php
+++ backend/user_upload.php
@@ -15,6 +15,12 @@ $commands = [
     '--help'            => 'which will output the above list of directives with details.'
 ];
 
+$results    = ['success' => [], 'errors' => []]; // Contains result of import process.
+$encoding   = 'UTF-8'; // Encoding used for parsing column.
+$separator  = ','; // Separator used to explode each line.
+$enclosure  = '"'; // Enclosure used to decorate each field.
+$maxRowSize = 4096; // Maximum row size to be used for decoding.
+
 $arguments = parseArguments($argv); // Fetch arguments.
 
 $dbConnection = getConnection($arguments); // Establish postgres connection.
@@ -27,8 +33,8 @@ $found = false; // Used to check for valid command line request.
 foreach($arguments as $key => $argument):
     switch($key):
         case 'create_table':
+            createTable($dbConnection);
             $found = true;
-
         break;
         case 'help':
             listCommands();
@@ -39,7 +45,8 @@ foreach($arguments as $key => $argument):
 
         break;
         case 'file':
-
+            importUsers($argument);
+            displayResults();
             $found = true;
         break;
         default:
@@ -49,7 +56,165 @@ endforeach;
 
 // List all commands if no option is selected.
 if(!$found)
-    listCommands();
+    listCommands();   
+
+function insertRecord($array)
+{
+    global $results, $dbConnection;
+    $name    = formatValue($array,'name');
+    $surname = formatValue($array,'surname');
+    $email   = formatValue($array,'email');
+
+    $result;
+    try {
+        $result = @pg_query($dbConnection, "SELECT email FROM users WHERE email = '$email'");
+        if(!$result) {
+            $array['message'] = "Some error occured fetching the record($email).";
+            $results['errors'][] = $array;    
+        }
+        $result = @pg_fetch_assoc($result);    
+        if(!$result) {
+            $array['message'] = "Some error occured fetching the record($email).";
+            $results['errors'][] = $array;    
+        }
+    }
+    catch(Exception $e)
+    {
+        $array['message'] = "Some error occured fetching the record($email).";
+        $results['errors'][] = $array;
+        return;
+    }
+
+    if($result) 
+    {
+        $array['message']    = $email. " already exists.";
+        $results['errors'][] = $array;
+    }
+    elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $array['message']    = $email. " is an invalid email.";
+        $results['errors'][] = $array;
+    }
+    else
+    {
+        $array = [
+            'name'    => $name,
+            'surname' => $surname,
+            'email'   => $email
+        ];
+        try {
+            $sql = "INSERT INTO users(name, surname, email) VALUES('$name','$surname','$email')";
+            pg_query($dbConnection, $sql);
+            $array['message'] = "$email successfully inserted.";
+            $results['success'][] = $array;    
+        }
+        catch(Exception $e)
+        {
+            $array['message'] = "Some error occured while inserting the record($email).";
+            $results['errors'][] = $array;    
+        }
+    }
+}    
+
+
+function displayResults()
+{
+    global $results, $successCode, $errorCode;
+    foreach($results as $key => $result)
+    {
+        if($key == 'success')
+            $colorCode = $successCode;
+        elseif($key == 'errors')
+            $colorCode = $errorCode;
+        foreach($result as $sResult)
+        {
+            echo "\033[".$colorCode."m".$sResult['message']."\033[0m\n";
+        }
+    }
+}
+
+function formatValue($array, $key)
+{
+    $value = isset($array[$key]) ? $array[$key] : '';
+    $value = strtolower($value);
+    $value = ucfirst($value);
+    return $value;
+}
+
+function convertEncoding($item) 
+{
+    global $encoding;
+    return iconv($encoding, "$encoding//IGNORE", $item);
+}
+    
+function parseArray($array) 
+{
+    $array = array_map('trim', $array);
+    $array = array_map('convertEncoding', $array);   
+    return $array;
+}
+
+// Parse the csv file
+function parseFile($filePath)
+{   
+    global $encoding, $separator, $enclosure, $maxRowSize;
+
+    $content = [];
+    $file = fopen($filePath, 'r');
+
+    $fields = fgetcsv($file, $maxRowSize, $separator, $enclosure);
+
+    while ( ($row = fgetcsv($file, $maxRowSize, $separator, $enclosure)) != false ) {
+        if ( count($row) < 3 ) // skip empty lines
+            continue;
+        $i = 0;
+        $tmp = [];
+        foreach($fields as $field)
+        {
+            $tmp[$field] = $row[$i];
+            $i++;
+        }
+        $content[] = parseArray($tmp);
+    }
+
+    fclose($file);
+    return $content;
+}
+    
+// Import users
+function importUsers($csv) 
+{
+    $filePath = dirname(__FILE__). "/$csv";
+
+    if(!file_exists($filePath)) // Check if the file exists.
+        die( "File does not exists $csv\n" );
+
+    $records  = parseFile($filePath);
+
+    foreach($records as $record)
+        insertRecord($record);
+}
+
+function createTable($connection)
+{
+    global $successCode, $errorCode;
+    $sql = "
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name CHARACTER VARYING(100) NULL,
+        surname CHARACTER VARYING(100) NULL,
+        email CHARACTER VARYING(100) NOT NULL UNIQUE
+    );
+    ";
+    echo "\033[".$successCode."mUser table has been created successfully.\033[0m\n";
+    try {
+        pg_query($connection, $sql);
+    }
+    catch(Exception $e) 
+    {
+        echo "\033[".$errorCode."mSome error occured while creating user table.\033[0m\n";
+    }
+    return true;
+}
 
 // Make database connection
 function getConnection(&$arguments)
